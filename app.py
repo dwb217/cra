e38c65947ff175bfd37ff3baf0942f963d93b334
dwb217 <davidwbender@gmail.com>
2019-10-05 15:18:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ note='1 is low or strong no; 5 is high or strong yes.'
 
 responses=[1,2,3,4,5]
 
-def calc(pick_score_q1,pick_score_q2,pick_score_q3):
-    score = 0
-    for x in score:
-        score +=x/len(args)
-    return score
-
 app.layout = html.Div(children=[
     html.H1(myheading1),
     html.Div([
@@ -36,36 +30,16 @@ app.layout = html.Div(children=[
                         ],
                 value='',
                 ),
-            html.H6(q_1),
-            dcc.Dropdown(
-                id='pick_score_q2',
-                options=[
-                        {'label':responses[0], 'value':responses[0]},
-                        {'label':responses[1], 'value':responses[1]},
-                        {'label':responses[2], 'value':responses[2]},
-                        {'label':responses[3], 'value':responses[3]},
-                        {'label':responses[4], 'value':responses[4]},
-                        ],
-                value='',
-                ),
-            html.H6(q_1),
-            dcc.Dropdown(
-                id='pick_score_q3',
-                options=[
-                        {'label':responses[0], 'value':responses[0]},
-                        {'label':responses[1], 'value':responses[1]},
-                        {'label':responses[2], 'value':responses[2]},
-                        {'label':responses[3], 'value':responses[3]},
-                        {'label':responses[4], 'value':responses[4]},
-                        ],
-                value='',
-                )
         ]
 
 @app.callback(Output('your_output_here', 'children'),
               [Input('pick_score_q1', 'value')]),
 html.H2(score)
-
+def calc(pick_score_q1,pick_score_q2,pick_score_q3):
+    score = 0
+    for x in score:
+        score +=x/len(args)
+    return score
 
 
 if __name__ == '__main__':
